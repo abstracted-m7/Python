8aeee336aee27d15c2376065a73b555bec64b985
Manish Giri <142075284+abstracted-m7@users.noreply.github.com>
2025-08-13 13:19:52 +0530

    Add files via upload

--- MCA-Python-Asst/P_N_Z.py
+++ MCA-Python-Asst/P_N_Z.py
@@ -1,11 +1,30 @@
-def num_senti(num):
-    if num == 0:
-        return "zero"
-    elif num < 0:
-        return "neg"
-    elif num > 0:
-        return "pos"
-    else:
-        return "Enter valid number"
+# def num_senti(num):
+#     if num == 0:
+#         return "zero"
+#     elif num < 0:
+#         return "neg"
+#     elif num > 0:
+#         return "pos"
+#     else:
+#         return "Enter valid number"
 
-print(num_senti(0))
+# print(num_senti(0))
+
+
+
+def num_senti(num):
+    try:
+        num_float = float(num)
+        if num_float == 0:
+            return "Zero"
+        elif num_float < 0:
+            return "Negative"
+        elif num_float > 0:
+            return "Positive"
+    except ValueError:
+        return "Please enter a valid number."
+
+
+user_input = input("Enter a number: ")
+print(num_senti(user_input))
+
